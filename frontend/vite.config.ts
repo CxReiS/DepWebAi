fd432901929f33f83f97e75aa5d78ef9cac3d8f9
CxReiS <cxteam1@gmail.com>
2025-07-28 15:58:57 +0300

    go-back

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://127.0.0.1:8000', // FastAPI backend yönlendirmesi
+      '/api': 'http://localhost:5173', // FastAPI backend yönlendirmesi
     }
   }
 })
