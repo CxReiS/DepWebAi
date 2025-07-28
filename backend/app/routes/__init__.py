f0cad4c9229c6b45127b22ff31f5568db1f915e3
CxReiS <cxteam1@gmail.com>
2025-07-28 03:32:26 +0300

    elle degisim

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,1 +1,6 @@
-"""API route paketleri."""
+from .auth import router as auth_router
+from .users import router as users_router
+from .models import router as models_router
+
+__all__ = ["auth_router", "users_router", "models_router"]
+
