b24caa005298674245deb6bacc71762014ca4967
CxReiS <cxteam1@gmail.com>
2025-07-30 14:08:07 +0300

    Add typed layouts with tests

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -2,19 +2,16 @@ import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import { ThemeProvider } from '../hooks/useTheme';
 import { ToastProvider } from '../hooks/useToast';
-import { StoreProvider } from '../store/store';
-
-export default function MainLayout({ children }) {
+import type { LayoutProps } from './LayoutProps';
+export default function MainLayout({ children }: LayoutProps) {
   return (
     <ThemeProvider>
       <ToastProvider>
-        <StoreProvider>
-          <Navbar />
-          <div className="main">
-            <Sidebar />
-            <div className="content">{children}</div>
-          </div>
-        </StoreProvider>
+        <Navbar />
+        <div className="main">
+          <Sidebar />
+          <div className="content">{children}</div>
+        </div>
       </ToastProvider>
     </ThemeProvider>
   );
